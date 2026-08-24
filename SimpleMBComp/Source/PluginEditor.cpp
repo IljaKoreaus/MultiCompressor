05ea0b09a45427a1c1e30c9cae2e6eeaeedc5d3e
Ilja Koreaus <iljakoskin@Ilja-Kannettava-tietokone.local>
2026-08-24 17:25:25 +0300

    Added Button, LnF & RotaryWithLabels Classes

--- SimpleMBComp/Source/PluginEditor.cpp
+++ SimpleMBComp/Source/PluginEditor.cpp
@@ -8,6 +8,232 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+void LookAndFeel::drawRotarySlider (juce::Graphics& g,
+                                         int x, int y, int width, int height,
+                                         float sliderPosProportional,
+                                         float rotaryStartAngle,
+                                         float rotaryEndAngle,
+                                         juce::Slider& slider)
+{
+    using namespace juce;
+    
+    auto bounds = Rectangle<float>(x, y, width, height);
+    
+    auto enabled = slider.isEnabled();
+    
+    g.setColour(enabled ? Colour(97u, 18u, 167u) : Colours::darkgrey);
+    g.fillEllipse(bounds);
+    
+    g.setColour(enabled ? Colour(255u, 154u, 1u) : Colours::grey);
+    g.drawEllipse(bounds, 1.f);
+    
+    if (auto* rswl = dynamic_cast<RotaryWithLabels*>(&slider)) {
+     
+        auto center = bounds.getCentre();
+        
+        Path p;
+        
+        Rectangle<float> r;
+        r.setLeft(center.getX()-2);
+        r.setRight(center.getX()+2);
+        r.setTop(bounds.getY());
+        r.setBottom(center.getY() - rswl->getTextHeight() * 2);
+        
+        p.addRoundedRectangle(r, 2.f);
+        
+        jassert(rotaryStartAngle < rotaryEndAngle);
+        
+        auto sliderAngRad = jmap(sliderPosProportional, 0.0f, 1.f, rotaryStartAngle, rotaryEndAngle);
+        
+        p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
+        
+        g.setColour(Colours::white);
+        g.fillPath(p);
+        
+        g.setFont(rswl->getTextHeight());
+        auto text = rswl->getDisplayString();
+
+        juce::GlyphArrangement glyphs;
+
+        glyphs.addLineOfText(g.getCurrentFont(), text, 0.0f, 0.0f);
+
+        auto strWidth = glyphs.getBoundingBox(0, -1, false).getWidth();
+        
+        r.setSize(strWidth + 4, rswl->getTextHeight() + 2);
+        r.setCentre(bounds.getCentre());
+        
+        g.setColour(enabled ? Colours::black : Colours::darkgrey);
+        g.fillRect(r);
+        
+        g.setColour(enabled ? Colours::white : Colours::lightgrey);
+        g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
+    }
+}
+
+void LookAndFeel::drawToggleButton(juce::Graphics &g,
+                                   juce::ToggleButton &toggleButton,
+                                   bool shouldDrawButtonAsHighlighted,
+                                   bool shouldDrawButtonAsDown)
+{
+    using namespace juce;
+    
+    if (auto* pb = dynamic_cast<PowerButton*>(&toggleButton))
+    {
+        Path powerButton;
+        auto bounds = toggleButton.getLocalBounds();
+        auto size = jmin(bounds.getWidth(), bounds.getHeight() - 6);
+        auto r = bounds.withSizeKeepingCentre(size, size).toFloat();
+        
+        float ang = 30.f;
+        
+        powerButton.addCentredArc(r.getCentreX(), r.getCentreY(),
+                                  size * 0.5,
+                                  size * 0.5,
+                                  0.f,
+                                  degreesToRadians(ang),
+                                  degreesToRadians(360-ang),
+                                  true);
+        
+        powerButton.startNewSubPath(r.getCentreX(), r.getY());
+        powerButton.lineTo(r.getCentre());
+        
+        PathStrokeType pst(2.f, PathStrokeType::JointStyle::curved);
+        
+        auto colour = toggleButton.getToggleState() ? Colours::dimgrey : Colours::green;
+        
+        g.setColour(colour);
+        g.strokePath(powerButton, pst);
+        g.drawEllipse(r, 2);
+        
+    } else if (auto* analyzerButton = dynamic_cast<AnalyzerButton*>(&toggleButton)) {
+        
+        auto colour = ! toggleButton.getToggleState() ? Colours::dimgrey : Colours::green;
+        g.setColour(colour);
+        
+        auto bounds = toggleButton.getLocalBounds();
+        g.drawRect(bounds);
+        
+        g.strokePath(analyzerButton->randomPath, PathStrokeType(1.f));
+    }
+}
+
+void RotaryWithLabels::paint(juce::Graphics& g)
+{
+    using namespace juce;
+    
+    auto startAng = degreesToRadians(180.f + 45.f);
+    auto endAng = degreesToRadians(180.f - 45.f) + MathConstants<float>::twoPi;
+    
+    auto range = getRange();
+    auto sliderBounds = getSliderBounds();
+    
+    getLookAndFeel().drawRotarySlider(g, sliderBounds.getX(),
+                                      sliderBounds.getY(),
+                                      sliderBounds.getWidth(),
+                                      sliderBounds.getHeight(),
+                                      jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0 ),
+                                      startAng,
+                                      endAng,
+                                      *this);
+    
+    auto center = sliderBounds.toFloat().getCentre();
+    auto radius = sliderBounds.getWidth() * 0.5f + getTextHeight() + 5;
+
+    g.setColour(Colour(0u, 172u, 1u));
+    g.setFont(getTextHeight());
+
+    auto numChoices = labels.size();
+
+    for (int i = 0; i < numChoices; i++)
+
+    {
+
+        auto pos = labels[i].pos;
+
+        jassert(0.f <= pos);
+        jassert(pos <= 1.f);
+
+        auto ang = jmap(pos, 0.0f, 1.0f, startAng, endAng);
+        auto c = center.getPointOnCircumference(radius, ang);
+        auto str = labels[i].label;
+
+        juce::GlyphArrangement glyphs;
+
+        glyphs.addLineOfText(g.getCurrentFont(), str, 0.0f, 0.0f);
+
+        auto strWidth = glyphs.getBoundingBox(0, -1, false).getWidth();
+
+        Rectangle<float> r;
+
+        r.setSize(strWidth + 4, getTextHeight() + 2);
+        r.setCentre(c);
+        g.drawFittedText(str,
+                         r.toNearestInt(),
+                         juce::Justification::centred,
+                         1
+        );
+
+    }
+}
+
+juce::Rectangle<int> RotaryWithLabels::getSliderBounds() const
+{
+    auto bounds = getLocalBounds();
+    auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
+    
+    size -= getTextHeight() * 2;
+    juce::Rectangle<int> r;
+    r.setSize(size, size);
+    r.setCentre(bounds.getCentreX(), 0);
+    r.setY(2);
+    
+    return r;
+}
+
+juce::String RotaryWithLabels::getDisplayString() const
+{
+    
+    if (auto* choiseParam = dynamic_cast<juce::AudioParameterChoice*>(param))
+    {
+        return choiseParam->getCurrentChoiceName();
+    }
+    
+    juce::String str;
+    auto addK = false;
+    
+    if (auto* floatParam = dynamic_cast<juce::AudioParameterFloat*>(param))
+    {
+        float val = getValue();
+        
+        if (val > 999.f) {
+            
+            val /= 1000.f;
+            addK = true;
+        }
+        
+        str = juce::String(val, (addK ? 2 : 0 ));
+        
+    } else {
+        
+        jassertfalse; // This shouldn't happen!!
+    }
+    
+    
+    if (suffix.isNotEmpty()) {
+        
+        str << " ";
+        if (addK) {
+            str << "k";
+        }
+        
+        str << suffix;
+    }
+    
+    return str;
+    
+}
+
+//==============================================================================
 
 Placeholder::Placeholder()
 {
