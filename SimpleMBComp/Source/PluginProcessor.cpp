9f7079ec3f37f8b7c30ff6df9f5d029213ecc6a7
Ilja Koreaus <iljakoskin@Ilja-Kannettava-tietokone.local>
2026-08-24 07:49:07 +0300

    Added Bypass Button

--- SimpleMBComp/Source/PluginProcessor.cpp
+++ SimpleMBComp/Source/PluginProcessor.cpp
@@ -34,6 +34,9 @@ SimpleMBCompAudioProcessor::SimpleMBCompAudioProcessor()
     
     ratio = dynamic_cast<juce::AudioParameterChoice*>(apvts.getParameter("Ratio"));
     jassert(ratio != nullptr);
+    
+    bypassed = dynamic_cast<juce::AudioParameterBool*>(apvts.getParameter("Bypassed"));
+    jassert(bypassed != nullptr);
 }
 
 SimpleMBCompAudioProcessor::~SimpleMBCompAudioProcessor()
@@ -176,6 +179,8 @@ void SimpleMBCompAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     auto block = juce::dsp::AudioBlock<float>(buffer);
     auto context = juce::dsp::ProcessContextReplacing<float>(block);
     
+    // TOGGLE BYPASS
+    context.isBypassed = bypassed->get();
     compressor.process(context);
 }
 
@@ -255,6 +260,10 @@ juce::AudioProcessorValueTreeState::ParameterLayout SimpleMBCompAudioProcessor::
                                                       "Ratio",
                                                       strarr,
                                                       3));
+    // BYPASS
+    layout.add(std::make_unique<AudioParameterBool>("Bypassed",
+                                                    "Bypassed",
+                                                    false));
     
     return layout;
 }
